da21f0a7388f5d33a926134cd23dd6b863e90326
Edson Michaque <edson@michaque.com>
2023-07-17 14:25:56 +0200

    Add connector management operations

---
+++ connector.go
@@ -0,0 +1,30 @@
+package openpds
+
+import (
+	"context"
+	"errors"
+)
+
+func (c Client) ListConnectors(ctx context.Context, input *CreateConnectorInput) (*CreateConnectorOutput, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (c Client) CreateConnector(ctx context.Context, input *CreateConnectorInput) (*CreateConnectorOutput, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (c Client) UpdateConnector(ctx context.Context, input *CreateConnectorInput) (*CreateConnectorOutput, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (c Client) GetConnector(ctx context.Context, input *CreateConnectorInput) (*CreateConnectorOutput, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (c Client) DeleteConnector(ctx context.Context, input *CreateConnectorInput) (*CreateConnectorOutput, error) {
+	return nil, errors.New("not implemented")
+}
+
+type CreateConnectorOutput struct{}
+
+type CreateConnectorInput struct{}
